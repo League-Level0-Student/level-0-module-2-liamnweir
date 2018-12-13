29e35747ee9cc25eccb062a339e5ddf84c427fb2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-12-12 16:26:11 -0800

    module 2 level 0

--- src/Checkpoint3.java
+++ src/Checkpoint3.java
@@ -1,14 +1,26 @@
 
 public class Checkpoint3 {
-public static void main(String[] args) {
-	 printMessage();
-	
-}
-static void printMessage() { 
-	
-        System.out.println("message");
-       
-   }
+	public static void main(String[] args) {
+		printMessage();
+		int r=getNumber();
+		setName();
+	}
 
-}
+	private static void setName() {
+		// TODO Auto-generated method stub
+		
+	}
 
+	static void printMessage() {
+
+		System.out.println("Hello");
+
+	}
+
+	static int getNumber() {
+		return number;
+	}
+	void setName(String name) { 
+        this.name = name;
+} 
+}
